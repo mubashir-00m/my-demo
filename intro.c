56fd108c6727b14e4162a64e43fd6ece7006873f
my-demo <mubashir002m@gmail.com>
2025-12-01 23:32:54 +0530

    this is a demo for push

---
+++ intro.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main ()
+{
+    int a=10;
+    int b=20;
+    int sum = a+b;
+    int sub = b-a;
+
+    printf("sum = %d\n",sum);
+    printf("sub = %d\n",sub);    
+
+    return 0;
+}
